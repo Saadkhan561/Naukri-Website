83e6515520bc8b3c71cf073345b0dec91b32aa8c
Saad Khan <saadnadeem885@gmail.com>
2023-01-06 06:22:01 -0800

    new commit

--- src/components/confirmAccount.jsx
+++ src/components/confirmAccount.jsx
@@ -3,231 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const ConfirmAccount = () => {
-  const [status, setStatus] = useState(true);
   const navigate = useNavigate();
-
-  // function setEmployeeStatus() {
-  //   setStatus(true);
-  // }
-  // function setEmployerStatus() {
-  //   setStatus(false);
-  // }
-
-  function RenderDiv() {
-    var x = document.getElementById('option-1');
-    if (x.selected === true) {
-      <DetailsDiv>
-        <Details>
-          <div>
-            <h1>Provide Details...</h1>
-          </div>
-          <div>
-            <Label>
-              <b>First Name</b>
-            </Label>
-            <Input type='text' name='fname' />
-          </div>
-          <div>
-            <Label>
-              <b>Last Name</b>
-            </Label>
-            <Input type='text' name='lname' />
-          </div>
-          <div>
-            <Label>
-              <b>Email</b>
-            </Label>
-            <Input type='email' name='email' />
-          </div>
-          <div>
-            <Label>
-              <b>Password</b>
-            </Label>
-            <Input type='password' name='password' />
-          </div>
-          <div>
-            <Label>
-              <b>Phone Number</b>
-            </Label>
-            <Input type='number' name='ph_no' />
-          </div>
-          <div>
-            <Label>
-              <b>Country</b>
-            </Label>
-            <Input type='text' name='country' />
-          </div>
-          <div>
-            <Label>
-              <b>City</b>
-            </Label>
-            <Input type='text' name='city' />
-          </div>
-          <SubmitButton onClick={() => navigate('/')}>Submit</SubmitButton>
-        </Details>
-      </DetailsDiv>;
-    } else {
-      <DetailsDiv>
-        <Details>
-          <div>
-            <h1>Provide Details...</h1>
-          </div>
-          <div>
-            <Label>
-              <b>Company Name</b>
-            </Label>
-            <Input type='text' name='cname' />
-          </div>
-          <div>
-            <Label>
-              <b>Email</b>
-            </Label>
-            <Input type='email' name='email' />
-          </div>
-          <div>
-            <Label>
-              <b>Password</b>
-            </Label>
-            <Input type='password' name='password' />
-          </div>
-          <div>
-            <Label>
-              <b>Phone Number</b>
-            </Label>
-            <Input type='number' name='ph_no' />
-          </div>
-          <div>
-            <Label>
-              <b>Country</b>
-            </Label>
-            <Input type='text' name='country' />
-          </div>
-          <div>
-            <Label>
-              <b>City</b>
-            </Label>
-            <Input type='text' name='city' />
-          </div>
-          <SubmitButton onClick={() => navigate('/')}>Submit</SubmitButton>
-        </Details>
-      </DetailsDiv>;
-    }
-  }
-
-  function Employee() {
-    return (
-      <DetailsDiv>
-        <Details>
-          <div>
-            <h1>Provide Details...</h1>
-          </div>
-          <div>
-            <Label>
-              <b>First Name</b>
-            </Label>
-            <Input type='text' name='fname' />
-          </div>
-          <div>
-            <Label>
-              <b>Last Name</b>
-            </Label>
-            <Input type='text' name='lname' />
-          </div>
-          <div>
-            <Label>
-              <b>Email</b>
-            </Label>
-            <Input type='email' name='email' />
-          </div>
-          <div>
-            <Label>
-              <b>Password</b>
-            </Label>
-            <Input type='password' name='password' />
-          </div>
-          <div>
-            <Label>
-              <b>Phone Number</b>
-            </Label>
-            <Input type='number' name='ph_no' />
-          </div>
-          <div>
-            <Label>
-              <b>Country</b>
-            </Label>
-            <Input type='text' name='country' />
-          </div>
-          <div>
-            <Label>
-              <b>City</b>
-            </Label>
-            <Input type='text' name='city' />
-          </div>
-          <SubmitButton onClick={() => navigate('/')}>Submit</SubmitButton>
-        </Details>
-      </DetailsDiv>
-    );
-  }
-
-  function Employer() {
-    return (
-      <DetailsDiv>
-        <Details>
-          <div>
-            <h1>Provide Details...</h1>
-          </div>
-          <div>
-            <Label>
-              <b>Company Name</b>
-            </Label>
-            <Input type='text' name='cname' />
-          </div>
-          <div>
-            <Label>
-              <b>Email</b>
-            </Label>
-            <Input type='email' name='email' />
-          </div>
-          <div>
-            <Label>
-              <b>Password</b>
-            </Label>
-            <Input type='password' name='password' />
-          </div>
-          <div>
-            <Label>
-              <b>Phone Number</b>
-            </Label>
-            <Input type='number' name='ph_no' />
-          </div>
-          <div>
-            <Label>
-              <b>Country</b>
-            </Label>
-            <Input type='text' name='country' />
-          </div>
-          <div>
-            <Label>
-              <b>City</b>
-            </Label>
-            <Input type='text' name='city' />
-          </div>
-          <SubmitButton onClick={() => navigate('/')}>Submit</SubmitButton>
-        </Details>
-      </DetailsDiv>
-    );
-  }
 
   return (
     <Main>
       <OptionDiv>
-        <Select>
-          <option id='opiton-1'>As an employer</option>
-          <option id='option-2'>As an employee</option>
-        </Select>
+        <OptionSubDiv>
+        <Employee>
+          As an employee
+        </Employee>
+        <Employer>
+          As an employer
+        </Employer>
+        </OptionSubDiv>
       </OptionDiv>
-      {console.log(status)}
-      {RenderDiv()}
+     {/* {renderDiv()} */}
       {/* <DetailsDiv>
         <Details>
         <div>
@@ -328,10 +118,6 @@ const ConfirmAccount = () => {
 
 export default ConfirmAccount;
 
-function checkStatus(props) {
-  props.status ? 'repeat(8, 70px) 50px;' : 'repeat(7, 70px) 50px;';
-}
-
 // STYLED CSS
 const Main = styled.div``;
 
@@ -340,19 +126,34 @@ const OptionDiv = styled.div`
   justify-content: center;
 `;
 
-const Select = styled.select`
-  width: 500px;
-  padding: 10px;
-  font-size: 17px;
-  margin-top: 40px;
-  transition: 400ms;
-  &:focus {
-    outline: none;
-    border: 1px solid rgb(71, 71, 242);
-    transition: 800ms;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+const OptionSubDiv = styled.div`
+  display: grid;
+  grid-template-columns: 320px 320px;
+  column-gap: 100px;
+`
+
+const Employee = styled.div`
+  margin-top: 30px;
+  padding: 30px 80px 10px;
+  border: 1px solid grey;
+  border-radius: 10px;
+  font-size: 20px;
+  &:hover {
+    background-color: #e0dddd;
+    transition: 400ms;
   }
-`;
+`
+const Employer = styled.div`
+border: 1px solid grey;
+padding: 30px 80px 30px;
+margin-top: 30px;
+border-radius: 10px;
+font-size: 20px;
+&:hover {
+  background-color: #e0dddd;
+  transition: 400ms;
+}
+`
 
 const DetailsDiv = styled.div`
   display: flex;
@@ -360,7 +161,7 @@ const DetailsDiv = styled.div`
 `;
 const Details = styled.form`
   display: grid;
-  grid-template-rows: ${checkStatus}
+  grid-template-rows: 'repeat(8, 70px) 50px;';
   width: 45%;
   margin-top: 40px;
 `;
